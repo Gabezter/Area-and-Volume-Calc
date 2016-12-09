e32f5eaeed985c8e9100fe8882467b561ee701d8
Gabe <Gabezter4@yahoo.com>
2016-12-09 09:15:28 -0600

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main extends JFrame implents ActionListener {
+
+	Area a;
+	Volume v;
+	
+	public void main(){
+		this.Main().setVisible(true);
+	}
+	
+	public Main(){
+		super("Area and Volume Calculator");
+	}
